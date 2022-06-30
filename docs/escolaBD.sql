09d878ddefba0543a8db3a90f81ccfa828889741
Jéssica Grimaldi <jessicagrimaldisa@gmail.com>
2022-06-30 15:00:13 -0300

    Insere dados nas tabelas Turma e Aluno
    Dados inseridos para facilitar a verificação via Postman/Swagger que deve ser feita após implementar CRUD e Regras de Negócio;

--- docs/escolaBD.sql
+++ docs/escolaBD.sql
@@ -1,4 +1,4 @@
--- ETAPA 01 DO PROJETO: CRIA«√O DE BANCO DE DADOS, TABELAS E DEFINI«√O DO RELACIONAMENTO
+-- ETAPA 01 DO PROJETO: CRIA√á√ÉO DE BANCO DE DADOS, TABELAS E DEFINI√á√ÉO DO RELACIONAMENTO
 CREATE DATABASE escola
 
 USE escola
@@ -17,3 +17,7 @@ Sexo CHAR NOT NULL,
 TotalFaltas INT,
 Id_Turma INT FOREIGN KEY REFERENCES Turma(Id)
 );
+
+INSERT INTO Turma VALUES ('Backend',1), ('Front End',1), ('Full stack',1), ('Mobile',0), ('Ci√™ncia de Dados',1)
+
+INSERT INTO Aluno VALUES ('J√©ssica Grimaldi','1992-11-29','F',0,2), ('Kami','1992-12-14','F',0,2), ('Rafa','1992-11-20','F',2,2), ('Robert','1993','M',0,3)
